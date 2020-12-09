ce4f61fc50deb39110cb053722a614078991b45f
drbdrb <drbdrb@163.com>
2020-12-09 09:47:22 +0800

    重写升级插件

--- python/plugin/Plug_in_Library/Plug_in_Library.py
+++ python/plugin/Plug_in_Library/Plug_in_Library.py
@@ -1,16 +1,24 @@
 from MsgProcess import MsgProcess, MsgType
 import time,os,re
 import RPi.GPIO as GPIO
-import qrcode#二维码模块
+
 
 class Plug_in_Library(MsgProcess):
 
     def Start(self,x):
         os.system("sudo chmod 777 plugin/Plug_in_Library")
         os.system("sudo pip3 install qrcode")
+        
 
     def Text(self, message):
+        
         try:
+            try:
+                import qrcode#二维码模块
+            except:
+                self.say("插件库启动失败，确保网络正常并且重新开机！")   
+                return 0
+
             if not "关闭" in message["Data"]:
                 ipAddrs = os.popen("hostname -I").read()   # 取IP地址
                 if ipAddrs:  # 如果有IP
